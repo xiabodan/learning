acb365b9f6cdb6dac925a68c97512c203d219091
xiabo <xiabodan@163.com>
2020-05-16 16:56:16 +0800

    java socket server updata.
    Change-Id: I22a2cbc8d666323079ec17156e374c9eab3b04eb

--- SocketServer.java
+++ SocketServer.java
@@ -2,11 +2,13 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServer {
     public static final int PORT = 12346;//监听的端口号 
+    public static int Name= 0;//监听的端口号
 
     public static void main(String[] args) {
         System.out.println("服务器启动...\n");
@@ -21,6 +23,7 @@ public class SocketServer {
                 // 一旦有堵塞, 则表示服务器与客户端获得了连接
                 Socket client = serverSocket.accept();
                 // 处理这次连接
+                System.out.println("服务器接收开始");
                 new HandlerThread(client);
             }
         } catch (Exception e) {
@@ -39,19 +42,27 @@ public class SocketServer {
             try {
                 // 读取客户端数据
                 DataInputStream input = new DataInputStream(socket.getInputStream());
-                String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
+                // String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
                 // 处理客户端数据
-                System.out.println("客户端发过来的内容:" + clientInputStr);
+                // System.out.println("客户端发过来的内容:" + clientInputStr);
 
                 // 向客户端回复信息
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                System.out.print("请输入:\t");
+                DataOutputStream out = new DataOutputStream(new FileOutputStream("A" + Name));
+                Name++;
+                byte[] buf = new byte[1027 * 9];
+                int len = 0;
+
+                while ((len = input.read(buf)) != -1) {
+                    out.write(buf, 0, len);
+                }
+                out.flush();
                 // 发送键盘输入的一行
-                String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-                out.writeUTF(s);
+                // String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                // out.writeUTF(s);
 
                 out.close();
                 input.close();
+                System.out.println("服务器接收完成");
             } catch (Exception e) {
                 System.out.println("服务器 run 异常: " + e.getMessage());
             } finally {
